cbb07470cbe2e7db9133634e45af9d12dde97d66
Julien Lambert <julien.lambert@professorbob.ai>
2022-11-25 09:21:37 +0100

    feat: composite version

---
+++ semantic-extract-version.cjs
@@ -0,0 +1,13 @@
+const fs = require("fs");
+
+const OUTPUT = process.env.GITHUB_OUTPUT ?? "output";
+
+module.exports = {
+  async verifyRelease(_, context) {
+    fs.appendFileSync(
+      OUTPUT,
+      `next-release=${!!context?.nextRelease?.version}
+			next-release-version=${context?.nextRelease?.version ?? ""}`
+    );
+  },
+};
